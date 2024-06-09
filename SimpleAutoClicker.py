1bcacea364787b04bd65d49a4272129352f31fbb
Yarovich <97020231+Yarovich@users.noreply.github.com>
2024-06-09 22:08:09 +0300

    Update SimpleAutoClicker.py

--- SimpleAutoClicker.py
+++ SimpleAutoClicker.py
@@ -1,6 +1,7 @@
-import keyboard
 from pynput.mouse import Controller, Button
-import time
+from keyboard import add_hotkey
+from time import sleep
+
 
 stop = False
 mouse = Controller()
@@ -12,8 +13,8 @@ def on_press():
     print(stop)
 
 
-keyboard.add_hotkey('f6', on_press)  # < горячая клавиша для вкл/выкл кликера
+add_hotkey('f6', on_press)  # < горячая клавиша для вкл/выкл кликера
 while True:
     if stop:
         mouse.click(Button.left, 1)  # < Button.left / Button.right / Button.middle
-    time.sleep(0.001)  # < задержка между кликами
+    sleep(0.001)  # < задержка между кликами
